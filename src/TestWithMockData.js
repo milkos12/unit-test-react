2bf843ca38ff4e06287b5f55b3d8dc262d2ff3cc
Miller Ostos <millerandresoc@gmail.com>
2024-07-11 11:56:38 -0500

    add test with user-event

--- src/TestWithMockData.js
+++ src/TestWithMockData.js
@@ -10,8 +10,13 @@ export const TestWithMockData = ({ data, displayUnorderedLIst, handleCick }) => 
                             {item.id}
                             {item.first_name},
                             {item.last_name},
-                            {item.email}
+                            <a onClick={()=> {
+                                console.log("email Click!!")
+                                handleCick()
+                            }}>{item.email}</a>
+                            {item.age > 50 ? 'Senior': 'Not sernior'}
                         </li>
+                        
 
                     ))}
                 </ul>
@@ -20,10 +25,15 @@ export const TestWithMockData = ({ data, displayUnorderedLIst, handleCick }) => 
                     {data.map(item => (
                         <li key={item.id}>
                             Last name: {item.last_name}
+                            
                         </li>
                     ))}
                 </ol>
             }
+            <label htmlFor='password'> Enter password</label>
+            <input type='password' id='password'/>
+            <input placeholder='Enter name'/>
+            <button> Submit </button>
         </div>
     )
 }
--- src/test/TesWithMockData.test.js
+++ src/test/TesWithMockData.test.js
@@ -2,35 +2,65 @@ import { render, screen } from "@testing-library/react";
 import { TestWithMockData } from "../TestWithMockData"
 
 const mockData = [
-    {
-        "id": 1,
-        "first_name": "Fletcher",
-        "last_name": "McVanamy",
-        "email": "mmcvanamy0@e-recht24.de",
-        "age": 30
-      }, {
-        "id": 2,
-        "first_name": "Clarice",
-        "last_name": "Harrild",
-        "email": "charrild1@dion.ne.jp",
-        "age": 35
-      }, 
-      {
-        "id": 3,
-        "first_name": "Amby",
-        "last_name": "Emmer",
-        "email": "aemmer2@buzzfeed.com",
-        "age": 67
-    }
+  {
+    "id": 1,
+    "first_name": "Fletcher",
+    "last_name": "McVanamy",
+    "email": "mmcvanamy0@e-recht24.de",
+    "age": 30
+  }, {
+    "id": 2,
+    "first_name": "Clarice",
+    "last_name": "Harrild",
+    "email": "charrild1@dion.ne.jp",
+    "age": 35
+  },
+  {
+    "id": 3,
+    "first_name": "Amby",
+    "last_name": "Emmer",
+    "email": "aemmer2@buzzfeed.com",
+    "age": 67
+  }
 ]
 
 test("LIst renders successfully", () => {
   render(<TestWithMockData data={mockData} />)
-  expect(screen.getByText(/McVanamy/i)).toBeInTheDocument();
+  expect(screen.getByText(/Fletcher/i)).toBeInTheDocument();
 })
 
 test("Order list renders", () => {
   render(<TestWithMockData data={mockData} displayUnorderedLIst={false} />)
 
   expect(screen.getByText(/McVanamy/)).toBeInTheDocument()
+})
+
+test("Email link handle called", async () => {
+  const handleCick = jest.fn();
+
+  render(<TestWithMockData
+    data={mockData}
+    displayUnorderedLIst={true}
+    handleCick={handleCick}
+  />)
+
+  await userEvent.click(screen.getByText(/mmcvanamy0@e-recht24.de/i));
+  expect(handleCick).toHaveBeenCalled();
+})
+
+
+test("Elements ", () => {
+  render(<TestWithMockData
+    data={mockData}
+    displayUnorderedLIst={true}
+    handleCick={handleCick}
+  />)
+
+  const input = screen.getByRole('textbox');
+  const button = screen.getByRole('button');
+
+  const nameInput = screen.getByPlaceholderText(/enter name/i)
+  const descriptionName = screen.getByPlaceholderText(/enter description/i)
+
+  
 })
