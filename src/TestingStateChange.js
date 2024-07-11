172636f2a43458f52ff749a905370965980cd788
Miller Ostos <millerandresoc@gmail.com>
2024-07-11 12:37:53 -0500

    add with state change

--- src/TestingStateChange.js
+++ src/TestingStateChange.js
@@ -2,11 +2,17 @@ import { useState, useEffect } from "react"
 
 export const TestingStateChange = () => {
     const [loaded, setLoad] = useState(false)
+    const [toogleTextVisble, setToggleVissble] = useState(false)
+
     useEffect(() => {setLoad(true)},[])
 
     return (
         <div>
             {loaded && <h3>Page loaded</h3>}
+            {toogleTextVisble && <p>Text visible</p>}
+            <button onClick={() => setToggleVissble(true)}>
+                Toogle text
+            </button>
         </div>
     )
 }
--- src/test/TestingWithMockData.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react"
-import { TestingStateChange } from "../TestingStateChange"
-
-test("TEsting page load", () => {
-    render(<TestingStateChange />)
-    expect(screen.getByText(/Page loaded/i)).toBeInTheDocument();
-})
